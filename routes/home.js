23062a5600725d754250049e2c6a693f685f8cb0
Stefan <stefantaitano@gmail.com>
2023-04-21 07:03:43 -0600

    fix route to create ticket

--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,7 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex)
-router.post('/', homeController.createTask)
+router.post('/', homeController.createTicket)
 
 
 module.exports = router
