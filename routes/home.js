742a6856b02080a06b31896fbf1f344c5f717aa1
Stefan <stefantaitano@gmail.com>
2023-04-23 21:12:02 -0600

    create new ejs files and fix bug in code. note: remove flash

--- routes/home.js
+++ routes/home.js
@@ -3,19 +3,19 @@
 
 const express = require('express')
 const router = express.Router()
-const authController = require("../controllers/auth");
+// const authController = require("../controllers/auth");
 const homeController = require('../controllers/home')
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+// const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.get('/', homeController.getIndex)
 router.post('/new', homeController.createTicket)
 
 //Authentication
-router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
-router.get("/logout", authController.logout);
-router.get("/signup", authController.getSignup);
-router.post("/signup", authController.postSignup);
+// router.get("/login", authController.getLogin);
+// router.post("/login", authController.postLogin);
+// router.get("/logout", authController.logout);
+// router.get("/signup", authController.getSignup);
+// router.post("/signup", authController.postSignup);
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,19 +3,18 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 const mongoose = require("mongoose");
-const passport = require("passport");
-const flash = require("express-flash");
+// const passport = require("passport");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 const homeRoutes = require("./routes/home")
 const editRoutes = require("./routes/edit")
-const authRoutes = require("./routes/auth")
+// const authRoutes = require("./routes/auth")
 
 //Use .env file in config folder
 require('dotenv').config({path: './config/.env'})
 
 // Passport config
-require("./config/passport")(passport);
+// require("./config/passport")(passport);
 
 connectDB()
 
@@ -25,18 +24,15 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 
 // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 //Logging
 app.use(logger("dev"));
 
-//Use flash messages for errors, info, ect...
-app.use(flash());
-
 //Set Routes
 app.use('/', homeRoutes)
-app.use('/auth', authRoutes)
+// app.use('/auth', authRoutes)
 app.use('/edit', editRoutes)
 
 
